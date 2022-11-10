4ba4683735a0ba2e8fa0e350ec2b789aacff413d
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-10 15:11:11 -0500

    Find, add, and delete thoughts in controllers folder

---
+++ controllers/toughts.js
@@ -0,0 +1,81 @@
+const Users = require('../models/users');
+const Thoughts = require('../models/thoughts');
+
+module.exports = {
+
+    oneThought(req, res) {
+        Thoughts.findOne({ _id: req.params.thoughtsId })
+        .then((thoughts) =>
+            !thoughts
+            ? res.status(404).json({ message: 'No thought found' })
+            : res.json(thoughts)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    gatherThoughts(req, res) {
+        Thoughts.find()
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    updateThought(req, res) {
+        Thoughts.findOneAndUpdate(
+        { _id: req.params.thoughtsId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+        )
+        .then((thoughts) =>
+            !thoughts
+            ? res.status(404).json({ message: 'Cannot find thought' })
+            : res.json(thoughts)
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    newThoughts(req, res) {
+        Thoughts.create(req.body)
+        .then((thoughts) => {
+            return Users.findOneAndUpdate(
+                { _id: req.body.usersId },
+                { $push: { thoughts: thoughts._id } },
+                { new: true }
+            );
+        })
+        .then((users) =>
+            !users
+            ? res.status(404).json({
+                message: 'No User found',
+                })
+            : res.json('Thought processed')
+        )
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+            });
+    },
+
+    deleteThoughts(req, res) {
+        Thoughts.findOneAndRemove({ _id: req.params.thoughtsId })
+        .then((thoughts) =>
+            !thoughts
+            ? res.status(404).json({ message: 'Cannot find thought' })
+            : Users.findOneAndUpdate(
+                { thoughts: req.params.thoughtsId },
+                { $pull: { thoughts: req.params.thoughtsId } },
+                { new: true }
+                )
+        )
+        .then((users) =>
+            !users
+            ? res
+            .status(404)
+            .json({ message: 'No user found' })
+            : res.json({ message: 'Lost thought' })
+        )
+        .catch((err) => res.status(500).json(err));
+    }
+};
